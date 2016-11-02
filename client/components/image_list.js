792fb45095bc26b85be7ea272924838f4b244bf7
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-02 18:06:10 +0300

    fetch images

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ImageDetail from './image_detail';
 
 
-const IMAGES = [
-  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
-  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
-  { title: 'Mug', link: 'http://dummyimage.com/600x400' }
-];
-
-const ImageList = () => {
-  const RenderedImages = IMAGES.map(function(image){
-    return <ImageDetail image={image} />
-  });
+const ImageList = (props) => {
+  const RenderedImages = props.images.map(image =>
+     <ImageDetail key={image.title} image={image} />
+  );
   return (
     <ul className="media-list list-group">
       {RenderedImages}
